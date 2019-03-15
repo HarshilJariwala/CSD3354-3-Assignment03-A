7f3b9b3f679ba83d2aca35070d2693cb13ae7738
visualstudio <visualstudio@122-PC14>
2019-03-15 19:47:27 -0400

    7:47 commit

---
+++ Bank/BankAccount.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Class1
+    {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+
+        public Class1()
+        { }
+
+        public Class1(string customerName, double balance) {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName {
+            get  { return m_customerName; }
+
+        }
+        public double balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount) {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if(amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            if(amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance -= amount;
+        }
+
+        public void credit(double amount)
+        {
+            if(m_frozen)
+            {
+                throw new ArgumentOutOfRangeException("Amount frozen");
+            }
+            if(amount<0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+
+        private void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+
+        public static void Main()
+        {
+            Class1 ba = new Class1("Mr. Bryan Walton", 11.99);
+
+            ba.credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.balance);
+        }
+    }
+}
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bank
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfrange()
+        {
+            double beginningBalance = 11.99;
+            double debitamount = -100.00;
+            //double expected = 7.44;
+
+            Class1 account = new Class1("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitamount);
+
+            double actual = account.balance;
+          //  Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+       [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsMoreThanZero_ShouldThrowArgumentOutOfrange()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 100.00;
+
+            Class1 account = new Class1("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            double actual = account.balance;
+        }
+    }
+}
