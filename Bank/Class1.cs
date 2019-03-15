1dac8ff766ba188ef9dd7c91370fd3a8e6d628f1
visualstudio <visualstudio@122-PC14>
2019-03-15 17:38:09 -0400

    5:37 commit

--- Bank/Class1.cs
+++ Bank/Class1.cs
@@ -8,5 +8,36 @@ namespace Bank
 {
     public class Class1
     {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+
+        private Class1()
+        { }
+
+        public Class1(string customerName, double balance) {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName {
+            get  { return m_customerName; }
+
+        }
+        public double balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount) {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if(amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+        }
     }
 }
